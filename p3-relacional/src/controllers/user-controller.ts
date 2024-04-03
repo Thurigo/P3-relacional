2f7a50ad26be4e8474786160bcd111e7db41b24a
Thurigo <arthur.spier@gmail.com>
2024-04-03 09:06:50 -0300

    ✅ upadate

--- p3-relacional/src/controllers/user-controller.ts
+++ p3-relacional/src/controllers/user-controller.ts
@@ -2,36 +2,33 @@ import { Response , Request } from "express";
 import { userRepositorio } from "../repositorio/user-repositorio";
 import { User } from "../entities/Users";
 import { Console } from "console";
+import { get } from "http";
 
 
 export class UserControler{
 
     async create(req: Request, res: Response){
       
-        //criar 
-        
-        const {User2} = req.body;
-        
-
-        
-
-
         try {
             const newUser = userRepositorio.create({nome:req.body.nome, email:req.body.email , peso:req.body.peso, senha:req.body.senha})
-
-
             await userRepositorio.save(newUser)
-
             console.log(newUser)
-
             return res.status(201).json(newUser)    
-
-
         }catch (error){
             return res.status(error)
-
         }
     }
 
+    async findOneByID (req: Request, res: Response){
+        try{
+            // const getUser = userRepositorio.findOneById({id:req.body.id})
+            console.table(req.body)
+            const usuario = await userRepositorio.findOneBy({id:req.body.id})
+            console.table(usuario)
+            return res.status(200).json(usuario)
 
+        }catch (error){
+            return res.status(error);
+        }
+    }
 }
--- p3-relacional/src/routes.ts
+++ p3-relacional/src/routes.ts
@@ -4,5 +4,6 @@ import { UserControler } from './controllers/user-controller'
 const routes = Router()
 
 routes.post('/user', new UserControler().create)
+routes.get('/get', new UserControler().findOneByID)
 
 export default routes
